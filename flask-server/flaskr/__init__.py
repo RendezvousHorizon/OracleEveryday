53eb3ea9d99c6b9c4491aa6e2f660e2db8f00280
wxd <1165521407@qq.com>
2021-04-22 21:17:57 +0800

    Fix a small bug.

--- flask-server/flaskr/__init__.py
+++ flask-server/flaskr/__init__.py
@@ -28,7 +28,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'CHello, World!'
+        return 'Hello, World!'
 
     from . import db
     db.init_app(app)
